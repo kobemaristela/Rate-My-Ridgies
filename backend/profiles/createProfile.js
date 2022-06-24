fa169ae2ba58a933537445b76b59f16f2b189f6f
kobemaristela-RL <kobe.maristela@ridgelineapps.com>
2022-06-24 14:56:04 -0700

    Updated Profiles

--- backend/profiles/createProfile.js
+++ backend/profiles/createProfile.js
@@ -3,7 +3,7 @@ import handler from "../util/handler";
 import dynamoDb from "../util/dynamodb";
 
 
-// {"name" : "mike", "role" : "P&E"}
+// {"name" : "mike", "role" : "P&E", "photo" : "test.jpg"}
 export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
 
