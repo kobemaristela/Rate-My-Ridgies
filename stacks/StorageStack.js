1f5536e2d8311dc97971d4fc4b6a54c033f9353b
kobemaristela-RL <kobe.maristela@ridgelineapps.com>
2022-06-24 11:30:28 -0700

    Updated StorageStack

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -2,24 +2,15 @@ import { Bucket, Table } from "@serverless-stack/resources";
 
 export function StorageStack({ stack }) {
   // Create an S3 bucket
-  const bucket = new Bucket(stack, "Uploads", {
-    cors: [
-      {
-        maxAge: "1 day",
-        allowedOrigins: ["*"],
-        allowedHeaders: ["*"],
-        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
-      },
-    ],
-  });
+  const bucket = new Bucket(stack, "Uploads");
 
   // Create the DynamoDB table
   const table = new Table(stack, "Profile", {
     fields: {
-      userId: "string",
-      noteId: "string",
+      profileId: "string",
+      reviewId: "string",
     },
-    primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+    primaryIndex: { partitionKey: "profileId", sortKey: "reviewId" },
   });
 
   // Return the bucket and table resources
